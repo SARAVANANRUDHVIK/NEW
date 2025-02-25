78d44168a1d901bd6c71596c84b7e30587c8b8a3
SARAVANANRUDHVIK <saravana26391@gmail.com>
2025-02-25 19:10:42 +0530

    name is changed

--- Kiruthu/daughter.java
+++ Kiruthu/daughter.java
@@ -7,7 +7,7 @@ public class daughter {
 	public static void main(String[] args) {
 		System.out.println("HI");
 		System.out.println("helllo world");
-		System.out.println("My name is Kiruthvika");
+		System.out.println("My name is Kiruthu");
 
 	}
 
